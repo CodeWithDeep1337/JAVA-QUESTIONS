b30f2422c5cbb0dad565f5a36b4417e19d6a97aa
Deep BuddhadeV <24010101032@darshan.ac.in>
2025-01-27 22:59:52 +0530

    Add files via upload

---
+++ Array/J_4_MaximumIndex.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class J_4_MaximumIndex {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       int ans=0;
+        int[] arr={1,2,4,6,1137};
+        for(int i=0;i<=4;i++){
+            
+            if(arr[i]>ans){
+              ans=arr[i];
+            }
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
