a0bedeb349ba0e00b81b4765c18765bf859205c4
Deep BuddhadeV <24010101032@darshan.ac.in>
2025-02-04 18:25:14 +0530

    Update ButterflyPattern.java

--- Pattern Printing/ButterflyPattern.java
+++ Pattern Printing/ButterflyPattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class J_13_ButterflyPattern {
+public class ButterflyPattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("enter your number:");
