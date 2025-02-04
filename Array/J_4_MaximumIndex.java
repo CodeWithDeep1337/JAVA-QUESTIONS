72aac945ce658c996d3e9360378d971859dd1bcf
Deep BuddhadeV <24010101032@darshan.ac.in>
2025-02-04 18:25:44 +0530

    Update J_4_MaximumIndex.java

--- Array/J_4_MaximumIndex.java
+++ Array/J_4_MaximumIndex.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class J_4_MaximumIndex {
+public class MaximumIndex {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
        int ans=0;
